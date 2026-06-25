9177e281cc999d0f13b444d0cf388921ae122666
Xinjie Zhang <114923662+BaochaiXue@users.noreply.github.com>
2026-06-25 01:22:53 -0400

    feat: define Demo v5 data keys

---
+++ demo_v5/data_keys.py
@@ -0,0 +1,9 @@
+REQUIRED_TIME_KEYS = (
+    "object_points", "object_colors", "object_visibilities",
+    "object_motions_valid", "controller_points",
+)
+OPTIONAL_TIME_KEYS = (
+    "asap_object_points_filled", "asap_surface_points", "asap_interior_points",
+    "object_recovered", "object_recovery_confidence", "controller_observed",
+    "controller_recovered", "controller_recovery_confidence",
+)
